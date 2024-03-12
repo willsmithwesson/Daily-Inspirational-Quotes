b4c93cbfeb3d7719c6594c57ff9f9cd23081f95d
willsmithwesson <will.smith.wesson@gmail.com>
2024-03-12 17:27:23 +0100

    script now fetches quotes based on the specified category (in this case: inspire). It checks if the API request was successful and if the API returned an error. If either of these checks fail, the script will print an error message and exit. Finally, the script saves the fetched quote to a file named quotes.txt

--- daily-inspirational-quotes.sh
+++ daily-inspirational-quotes.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
 
+# Specify the category
+CATEGORY="inspire"
+
 # Fetch a random quote from an online API
-QUOTE=$(curl -s https://api.quotable.io/random)
+QUOTE=$(curl -s https://api.quotable.io/random?tags=$CATEGORY)
+
+# Check if the API request was successful
+if [ $? -ne 0 ]; then
+    echo "Error: Unable to fetch quote from API"
+    exit 1
+fi
+
+# Check if the API returned an error
+if echo $QUOTE | jq -e .error > /dev/null 2>&1; then
+    echo "Error: API returned an error - $(echo $QUOTE | jq -r '.error')"
+    exit 1
+fi
 
 # Extract the content and author from the JSON response
 CONTENT=$(echo $QUOTE | jq -r '.content')
@@ -9,3 +24,6 @@ AUTHOR=$(echo $QUOTE | jq -r '.author')
 
 # Display the quote
 echo "\"$CONTENT\" - $AUTHOR"
+
+# Save the quote to a file
+echo "\"$CONTENT\" - $AUTHOR" >> quotes.txt
