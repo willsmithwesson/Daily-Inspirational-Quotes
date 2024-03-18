ed9a1c71de60e93bdb904b8090caf61b7c5a6396
willsmithwesson <will.smith.wesson@gmail.com>
2024-03-18 22:55:55 +0100

    optimized code reduces the lines of code and makes it more maintainable.

--- daily-inspirational-quotes.sh
+++ daily-inspirational-quotes.sh
@@ -1,54 +1,41 @@
 #!/bin/bash
 
+fetch_and_save_quote() {
+    URL=$1
+    CONTENT_PATH=$2
+    AUTHOR_PATH=$3
+
+    # Fetch a quote from an online API
+    QUOTE=$(curl -s $URL)
+
+    # Check if the API request was successful
+    if [ $? -ne 0 ]; then
+        echo "Error: Unable to fetch quote from API"
+        exit 1
+    fi
+
+    # Check if the API returned an error
+    if echo $QUOTE | jq -e .error > /dev/null 2>&1; then
+        echo "Error: API returned an error - $(echo $QUOTE | jq -r '.error')"
+        exit 1
+    fi
+
+    # Extract the content and author from the JSON response
+    CONTENT=$(echo $QUOTE | jq -r $CONTENT_PATH)
+    AUTHOR=$(echo $QUOTE | jq -r $AUTHOR_PATH)
+
+    # Display the quote
+    echo "\"$CONTENT\" - $AUTHOR"
+
+    # Save the quote to a file
+    echo "\"$CONTENT\" - $AUTHOR" >> quotes.txt
+}
+
 # Specify the category
 CATEGORY="inspire"
 
-# Fetch a random quote from an online API
-QUOTE=$(curl -s https://api.quotable.io/random?tags=$CATEGORY)
+# Fetch and save a random quote
+fetch_and_save_quote "https://api.quotable.io/random?tags=$CATEGORY" '.content' '.author'
 
-# Check if the API request was successful
-if [ $? -ne 0 ]; then
-    echo "Error: Unable to fetch quote from API"
-    exit 1
-fi
-
-# Check if the API returned an error
-if echo $QUOTE | jq -e .error > /dev/null 2>&1; then
-    echo "Error: API returned an error - $(echo $QUOTE | jq -r '.error')"
-    exit 1
-fi
-
-# Extract the content and author from the JSON response
-CONTENT=$(echo $QUOTE | jq -r '.content')
-AUTHOR=$(echo $QUOTE | jq -r '.author')
-
-# Display the quote
-echo "\"$CONTENT\" - $AUTHOR"
-
-# Save the quote to a file
-echo "\"$CONTENT\" - $AUTHOR" >> quotes.txt
-
-# Fetch quote of the day from theysaidso API
-QUOTE_OF_DAY=$(curl -s https://api.theysaidso.com/qod.json)
-
-# Check if the API request was successful
-if [ $? -ne 0 ]; then
-    echo "Error: Unable to fetch quote of the day from API"
-    exit 1
-fi
-
-# Check if the API returned an error
-if echo $QUOTE_OF_DAY | jq -e .error > /dev/null 2>&1; then
-    echo "Error: API returned an error - $(echo $QUOTE_OF_DAY | jq -r '.error')"
-    exit 1
-fi
-
-# Extract the content and author from the JSON response
-CONTENT_OF_DAY=$(echo $QUOTE_OF_DAY | jq -r '.contents.quotes[0].quote')
-AUTHOR_OF_DAY=$(echo $QUOTE_OF_DAY | jq -r '.contents.quotes[0].author')
-
-# Display the quote of the day
-echo "\"$CONTENT_OF_DAY\" - $AUTHOR_OF_DAY"
-
-# Save the quote of the day to a file
-echo "\"$CONTENT_OF_DAY\" - $AUTHOR_OF_DAY" >> quotes.txt
+# Fetch and save quote of the day
+fetch_and_save_quote "https://api.theysaidso.com/qod.json" '.contents.quotes[0].quote' '.contents.quotes[0].author'
